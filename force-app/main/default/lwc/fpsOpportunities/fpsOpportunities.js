d25ed40d603d76f677632a2e72186d8ee5c714eb
Ashveeni Upasani <ashveeni@Ashveenis-MacBook-Air.local>
2023-10-05 10:52:24 -0400

    Changes to Opportunities

--- force-app/main/default/lwc/fpsOpportunities/fpsOpportunities.js
+++ force-app/main/default/lwc/fpsOpportunities/fpsOpportunities.js
@@ -1,27 +1,63 @@
 import { LightningElement, wire, track, api} from 'lwc';
 import getOpportunityByRecordTypeId from '@salesforce/apex/Opportunities.getOpportunityByRecordTypeId';
+import { getRecord } from 'lightning/uiRecordApi';
+import {refreshApex} from '@salesforce/apex';
+import USER_ID from '@salesforce/user/Id';
 
 export default class FpsOpportunities extends LightningElement
 {
      //Display Opportunities 
      @track oppList;
+     @track opps;
      @api recordId;
+     profilename = undefined;
+
+     @track showInlineEdits = false;
 
      @track columns = 
      [
-         { label: 'Name', fieldName: 'Name' },
-         { label: 'Amount ', fieldName: 'Amount' },    
-         { label: 'Stage', fieldName: 'StageName' },
-         { label: 'Close Date', fieldName: 'CloseDate' },
+         { label: 'Name', fieldName: 'Name'},
+         { label: 'Amount ', fieldName: 'Amount', editable : this.showInlineEdits },    
+         { label: 'Stage', fieldName: 'StageName' , editable : this.showInlineEdits },
+         { label: 'Close Date', fieldName: 'CloseDate', editable : this.showInlineEdits  },
            
      ];
 
-     @wire (getOpportunityByRecordTypeId, {recordId : '$recordId', recordTypeId :'0128c000002wSvuAAE' }) 
-     wiredopps({data, error})
+     @wire (getOpportunityByRecordTypeId, {recordId : '$recordId', recordTypeId :'0128c000002wSvuAAE' }) wiredopps(result)
      {
-         if(data)
-             this.oppList = data;
-         else if (error)
-              console.log(error);      
+        this.opps= result;
+
+        if(result.data)
+        {
+            this.oppList = result.data;
+        }
+        else if (result.error)
+            console.log(result.error); 
+             
+              
+     }
+
+     @wire (getRecord, {recordId: USER_ID, fields: ['User.Profile.Name']}) getprofileDetails ({error,data})
+     {
+        console.log('In profile wire');
+        if(data)
+        {
+            this.profilename = data.fields.Profile.displayValue;
+
+            if(this.profilename == 'System Administrator')
+            {
+                console.log('You have edit access');
+                this.showInlineEdits = true;
+                refreshApex(this.oppList);
+                console.log('refreshed');
+            }
+            else
+            {
+                console.log('You should not edit this .');
+            }
+        }
+        else if(error)
+            console.log('error');
+        
      }
 }
